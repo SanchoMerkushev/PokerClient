25615def32b0afc9468195415ca08b2806f119cc
Sancho <sashamerkushev@yandex.ru>
2024-04-25 03:53:15 +0300

    Create file with constants

---
+++ constants.py
@@ -0,0 +1,8 @@
+START_BALANCE = 1000
+CARDS_ON_TABLE = 5
+CARDS_ON_HAND = 2
+AMOUNT_RANKS = 13
+AMOUNT_SUITS = 4
+ALL_CARDS = [(rank, suit) for rank in range(AMOUNT_RANKS) for suit in range(AMOUNT_SUITS)]
+COMBINATIONS = ["High Card", "Pair", "Two Pair", "3 of a Kind", "Straight", "Flush", \
+                "Full House", "4 of a Kind", "Straight Flush", "Royal Flush"]
