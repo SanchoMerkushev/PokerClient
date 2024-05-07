9283771927866296998b46ddd8ddea8b2e40c983
iemttt <s02190454@gse.cs.msu.ru>
2024-05-07 10:42:10 +0300

    Add "UI"

--- client.py
+++ client.py
@@ -3,21 +3,53 @@ import socket
 import json
 import argparse
 import cmd
+import os
 from time import sleep
 
 from misc import recv_end, END
 
 
-def print_cards(cards):
+def cards_to_str(cards):
     """Print cards with Unicode."""
+    res = []
     for rank, suit in cards:
         if rank == "X" and suit == "X":
             code = 0x1F0A0
         else:
             rank = (rank + 1) % 13
             code = 0x1F0A1 + rank + suit * 0x10
-        print(chr(code), end="  ")
-    print()
+        res.append(chr(code))
+    return "  ".join(res)
+
+
+class UI():
+    """Class for printing game information."""
+
+    def print_state(self, state, my_name, my_cards):
+        """Print all game info."""
+        players_names = "Players: \t"
+        players_balances = "Balances:\t"
+        players_cards = "Cards:   \t"
+        players_bids = "Bids:    \t"
+        players_turns = "Turns:   \t"
+        for name in state["players"].keys():
+            players_names += name + "\t"
+            players_balances += str(state["players"][name]["balance"]) + "\t"
+            players_bids += str(state["players"][name]["bid"]) + "\t"
+            if name == my_name:
+                players_cards += cards_to_str(my_cards) + "\t"
+            else:
+                players_cards += cards_to_str(state["players"][name]["cards"]) + "\t"
+            if state["players"][name]["turn"]:
+                players_turns += "*" + "\t"
+            else:
+                players_turns += " " + "\t"
+        center = "\t"*((len(state["players"])*2 - 1)//2 - 1)
+        bank = center + f"Bank: {state['sum_bids']}"
+        cards = center + cards_to_str(state["visible_cards"])
+        os.system("clear")
+        ui = f"{players_names}\n{players_balances}\n{players_cards}\n{players_bids}\n{players_turns}\n\n{bank}\n{cards}"
+        print(ui)
 
 
 class Client(cmd.Cmd):
@@ -27,41 +59,34 @@ class Client(cmd.Cmd):
         super().__init__()
         self.s = socket.create_connection((host, port))
         self.name = name
+        self.prompt = f"{name}>"
         self.s.sendall(f"{name}{END}".encode())
         self.my_turn = False
+        self.ui = UI()
+        self.cards = None
         self.play()
 
     def play(self):
         """Communicate with server, main loop for client."""
         while True:
             self.game_state = json.loads(recv_end(self.s, END))
+            if "sync" in self.game_state:
+                self.ui.print_state(self.game_state, self.name, self.cards)
             if "game_is_over" in self.game_state:
                 self.s.close()
                 exit()
             if "cards_on_hand" in self.game_state:
-                output_string = "Your cards is"
-                print(output_string)
-                print_cards(self.game_state["cards_on_hand"])
-            if "balance" in self.game_state:
-                output_string = "Your balance is {}".format(self.game_state["balance"])
-                print(output_string)
-            if "common_cards" in self.game_state:
-                output_string = "Cards on table is"
-                print(output_string)
-                print_cards(self.game_state["common_cards"])
-            if "total_bids" in self.game_state:
-                output_string = "Total bids in this round {}".format(self.game_state["total_bids"])
-                print(output_string)
+                self.cards = self.game_state["cards_on_hand"]
             if "output_inf" in self.game_state:
                 output_string = self.game_state["output_inf"]
                 print(output_string)
-            sleep(0.1)
+                sleep(0.2)
             if "answer" in self.game_state and self.game_state["answer"]:
                 self.my_turn = True
                 break
 
     def do_raise(self, arg):
-        """Raise bet."""
+        """Raise bid."""
         if not self.my_turn:
             print("It's not your turn.")
         try:
@@ -73,10 +98,11 @@ class Client(cmd.Cmd):
             return
         command = f"RAISE {arg}{END}"
         self.s.sendall(command.encode())
+        self.my_turn = False
         self.play()
 
     def do_call(self, arg):
-        """Call bet."""
+        """Call bid."""
         if not self.my_turn:
             print("It's not your turn.")
         if arg != "":
@@ -85,6 +111,7 @@ class Client(cmd.Cmd):
             return
         command = f"CALL{END}"
         self.s.sendall(command.encode())
+        self.my_turn = False
         self.play()
 
     def do_fold(self, arg):
@@ -96,7 +123,12 @@ class Client(cmd.Cmd):
             return
         command = f"FOLD{END}"
         self.s.sendall(command.encode())
+        self.my_turn = False
         self.play()
+
+    def default(self, arg):
+        """Print error."""
+        print("Unknown command:", arg)
 
 
 if __name__ == '__main__':
