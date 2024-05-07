fbd3ce0412f85d6b677eaac4d3fca600d1355c60
Sancho <sashamerkushev@yandex.ru>
2024-05-07 16:45:04 +0300

    Add testing of transform cards to Unicode image

---
+++ src/test_cards_to_str.py
@@ -0,0 +1,22 @@
+"""Module for testing cards_to_str."""
+import unittest
+
+from client import cards_to_str
+from constants import RANKS, SUITS
+
+
+class TestCardsToStr(unittest.TestCase):
+    """Class to test cards_to_str."""
+
+    def setUp(self):
+        """Set up for testing."""
+        self.cards = ((RANKS.index("A"), SUITS.index("S")), (RANKS.index("8"), SUITS.index("D")))
+        self.unicode_symbols = chr(0x1F0A1) + "  " + chr(0x1F0C8)
+
+    def test_change_balance_after_winning(self):
+        """Test changing of balance."""
+        self.assertEqual(cards_to_str(self.cards), self.unicode_symbols)
+
+
+if __name__ == "__main__":
+    unittest.main()
